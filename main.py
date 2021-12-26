fdb17407e0108f16dc848da79ea308258622debf
IdotMaster1 <justapersom@gmail.com>
2021-12-26 23:22:17 +0100

    h

---
+++ main.py
@@ -0,0 +1,4 @@
+from web3 import Web3
+
+# Connect to to the RPC node running on polygon-rpc.com
+w3 = Web3(Web3.HTTPProvider("http://polygon-rpc.com"))
---
+++ website/README.md
@@ -0,0 +1,3 @@
+A website with some JS to communicate with the python "server"
+
+USE NGINX OR SOMETHING TO USE THIS 
