4721c1d325b0e11877c858200725de4d8a7583cc
sys-256 <sys-256@users.noreply.github.com>
2022-01-27 16:51:29 +0000

    deploy: a0b29f97b72a9b608ae9b0a582c605ccc39580b6

--- main.js
+++ main.js
@@ -1,1 +1,1 @@
-"use strict";console.log("sus");
+"use strict";const polygonRegex=/^[0-9a-zA-Z]{40,40}/g,submit_buttons=document.querySelectorAll("#submit");submit_buttons.forEach(async t=>{var e=t.parentElement?.id;"1"===e&&t.addEventListener("click",async t=>{const e=submit_buttons[0].parentElement.children[1];e.value&&42===e.value.length&&polygonRegex.test(e.value.slice(2)),submit_buttons[0].parentElement.style.display="none",submit_buttons[1].parentElement.style.display="block"})});
