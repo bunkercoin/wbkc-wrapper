be46c38b532442a260987bec03600c8e621f5167
Bram <84628826+sys-256@users.noreply.github.com>
2022-01-27 15:18:10 +0100

    Delete website directory

--- website/README.md
+++
@@ -1,3 +0,0 @@
-A website with some JS to communicate with the python "server"
-
-USE NGINX OR SOMETHING TO USE THIS 
--- website/js/index.js
+++
@@ -1,4 +0,0 @@
-// Copyright (c) 2021 The Bunkercoin Developers
-// Distributed under the MIT software license, see the accompanying
-// file COPYING or http://www.opensource.org/licenses/mit-license.php. 
- 
