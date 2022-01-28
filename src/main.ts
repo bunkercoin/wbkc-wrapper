dd9fbb0a15e5121433d07598cc77c06ff6ce28fa
Bram <84628826+sys-256@users.noreply.github.com>
2022-01-28 15:44:36 +0100

    main.ts: more wrapping code + cleaner code + more variables and comments

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,50 @@
-const polygonRegex: RegExp = /^[0-9a-zA-Z]{40,40}/g;
-const submit_buttons = document.querySelectorAll("#submit");
+// Config  variables
+const polygonAddressRegex: RegExp = /^[0-9a-zA-Z]{40,40}/g;
+const apiURL: string = `https://wrap.bunkercoin.xyz/api/`;
+const minDeposit: number = 0.01;
+const maxDeposit: number = 100;
+const minConfirmations: number = 3;
 
-submit_buttons.forEach(async (button: Element) => {
-    const stage: string | undefined = button.parentElement?.id;
-    
-    if (stage === "1") {
-        button.addEventListener("click", async (event: Event) => {
-            const input_field = <HTMLInputElement>submit_buttons[0].parentElement!.children[1];
+// Buttons labeled submit
+const submitButton1: Element | undefined | null = document.querySelector(`#submit1`);
+const submitButton2: Element | undefined | null = document.querySelector(`#submit2`);
+const submitButton3: Element | undefined | null = document.querySelector(`#submit3`);
 
-            if (input_field.value && input_field.value.length === 42) {
-                const validPolygonAddress: boolean = polygonRegex.test(input_field.value.slice(2));
-            }
+submitButton1?.addEventListener(`click`, async (event: Event) => { // User clicks on submit button after entering a matic address (Step 1)
+    const input_field = document.querySelector(`#stage1`)!.children[1] as HTMLInputElement;
 
-            submit_buttons[0].parentElement!.style.display = "none";
-            submit_buttons[1].parentElement!.style.display = "block";
-        });
-    } else if (stage === "2") {
+    if (input_field.value && input_field.value.length === 42 && polygonAddressRegex.test(input_field.value.slice(2))) {
+        // Get a desposit address from the API
+        const response: Response = await fetch(`${apiURL}getDepositAddress/${input_field.value}`);
+        const data = await response.json();
 
-    } else if (stage === "3") {
+        // Check if the address is valid
+        if (data[`message`][`addressMatic`] === input_field.value) {}
 
+        // Continue to step 2
+        (<HTMLElement>document.querySelector(`#stage1`)!).style.display = `none`;
+        (<HTMLElement>document.querySelector(`#stage2`)!).style.display = `block`;
+    } else {
+        alert(`Please enter a valid polygon address`);
+        return;
+    }
+});
+
+submitButton2?.addEventListener(`click`, async (event: Event) => {
+    const input_field = document.querySelector(`#stage2`)!.children[1] as HTMLInputElement;
+
+    if (input_field.value && input_field.value.length === 42) {
+        const validPolygonAddress: boolean = polygonAddressRegex.test(input_field.value.slice(2));
+    }
+
+    (<HTMLElement>document.querySelector(`#stage2`)!).style.display = `none`;
+    (<HTMLElement>document.querySelector(`#stage3`)!).style.display = `block`;
+});
+
+submitButton3?.addEventListener(`click`, async (event: Event) => {
+    const input_field = document.querySelector(`#stage3`)!.children[1] as HTMLInputElement;
+
+    if (input_field.value && input_field.value.length === 42) {
+        const validPolygonAddress: boolean = polygonAddressRegex.test(input_field.value.slice(2));
     }
 });
