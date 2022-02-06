cd4d354b4b10985bf23b99cb080aac820b83d29f
Bram <84628826+sys-256@users.noreply.github.com>
2022-02-06 20:12:01 +0100

    merge all typescript files together to index.ts

--- src/definitions.ts
+++
@@ -1,1 +0,0 @@
-const web3 = (window as any).ethereum;
--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,49 @@ const apiURL: string = `https://wrap.bunkercoin.xyz/api/`;
 const minDeposit: number = 100;
 const minConfirmations: number = 60;
 
+const web3 = (window as any).ethereum;
+
+// I hate this but else typescript will complain
+let Web3Eth: any;
+let eth: any;
+let ethereum: any;
+
+const addToMetaMask = async (): Promise<[string | undefined, boolean]> => {
+    if (ethereum !== undefined) {
+        eth = new Web3Eth(ethereum);
+    } else if (web3 !== undefined) {
+        eth = new Web3Eth(web3.givenProvider);
+    } else {
+        eth = new Web3Eth(ethereum);
+    }
+
+    if (eth !== undefined) {
+        const netID: number = await eth.net.getId();
+        if (netID == 137) {
+            return [undefined, true];
+        } else {
+            const params = [{
+                chainId: `0x89`,
+                chainName: `Matic Mainnet`,
+                nativeCurrency: {
+                    name: `MATIC`,
+                    symbol: `MATIC`,
+                    decimals: 18
+                },
+                rpcUrls: [`https://polygon-rpc.com/`],
+                blockExplorerUrls: [`https://polygonscan.com/`]
+            }];
+
+
+            return (window as any).ethereum.request({ method: `wallet_addEthereumChain`, params })
+                .then(() => [undefined, true]) // error = false, success = true
+                .catch((error: Error) => [error.message, false]); // error = error message, success = false
+        }
+    } else {
+        return [`Unable to locate MetaMask`, false];
+    }
+}
+
 // Buttons
 const metaMaskButton = document.getElementById(`button-add-metamask`) as HTMLButtonElement;
 const submitButton1 = document.querySelector(`#button-deposit-address`) as HTMLButtonElement;
@@ -13,7 +56,7 @@ const submitButton3 = document.querySelector(`#button-emit`) as HTMLButtonElemen
 
 (async () => {
     // Add the Polygon network and enable MetaMask
-    const [error, success] = await addNetwork();
+    const [error, success] = await addToMetaMask();
     if (error && !success) {
         alert(error);
     }
