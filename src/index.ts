176f646d7f9c1352680855da95e42c816134d443
Bram <84628826+sys-256@users.noreply.github.com>
2022-02-13 14:50:43 +0100

    index:ts verify signature + 2.5 min -> 5 min

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ const config = {
     polygonAddressRegex: /^[0][x][0-9a-zA-Z]{40}$/,
     bunkercoinAddressRegex: /^[B][a-zA-Z0-9]{33}$/,
     minDeposit: 10000,
-    apiURL: `https://wrap.bunkercoin.xyz/api/`
+    apiURL: `http://62.155.193.51:5000`,
+    explorerURL: `https://explorer.bunkercoin.xyz`,
 };
 
 // Define web3
@@ -66,14 +67,21 @@ const submitButton3 = document.querySelector(`#button-emit`) as HTMLButtonElemen
     });
 
     // Show the MetaMask address
-    //@ts-ignore - to ignore Web3Utils name to found
+    //@ts-ignore - to ignore Web3Utils name not found
     const checksummedAddress = Web3Utils.toChecksumAddress(web3.selectedAddress);
     (document.querySelector(`#matic-address`) as HTMLSpanElement).innerText = checksummedAddress;
 
     // Get a deposit address
     const data_deposit = await (await fetch(`${config.apiURL}/getDepositAddress/${checksummedAddress}`)).json();
     const message = JSON.parse(data_deposit.message);
-    const { node, signature } = data_deposit; // TODO: check signature
+    const { node, signature } = data_deposit;
+    
+    // Verify the signature
+    const verified = await (await fetch(`${config.explorerURL}/?action=verify&verify_address=${node}&verify_signature=${signature}&verify_message=${data_deposit.message}`)).json();
+    if (verified.success && !verified.result) {
+        alert(`The signature is invalid.`);
+        return;
+    }
     (document.querySelector(`#wrap-deposit-address`) as HTMLParagraphElement).innerText = message.depositAddress;
 
     const confirmed_data = (await (await fetch(`${config.apiURL}/getBalance/${checksummedAddress}`)).text()) // Get the text data from the response
@@ -89,7 +97,7 @@ const submitButton3 = document.querySelector(`#button-emit`) as HTMLButtonElemen
         }
     }
 
-    // Check every 2.5 minutes if the user desposited any (un)confirmed funds (2.5 min to avoid 429)
+    // Check every 5 minutes if the user desposited any (un)confirmed funds (5 min to avoid 429)
     setInterval(async () => {
         const confirmed_data = (await (await fetch(`${config.apiURL}/getBalance/${checksummedAddress}`)).text()) // Get the text data from the response
             .slice(1).slice(0, -1); // Remove the first and last " from the string
@@ -104,14 +112,14 @@ const submitButton3 = document.querySelector(`#button-emit`) as HTMLButtonElemen
             }
         }
 
-        const unconfirmed_data = (await (await fetch(`${config.apiURL}/getBalance/${checksummedAddress}`)).text()) // Get the text data from the response
+        const unconfirmed_data = (await (await fetch(`${config.apiURL}/getBalanceUnconfirmed/${checksummedAddress}`)).text()) // Get the text data from the response
             .slice(1).slice(0, -1); // Remove the first and last " from the string
 
         // Show the balance 
         if (unconfirmed_data !== (document.querySelector(`#wrap-unconfirmed`) as HTMLParagraphElement).innerText) {
             (document.querySelector(`#wrap-unconfirmed`) as HTMLParagraphElement).innerText = unconfirmed_data;
         }
-    }, 150 * 1000);
+    }, 300 * 1000);
 
     // If the user clicks the emit button
     (document.querySelector("#button-emit") as HTMLButtonElement).addEventListener("click", async (event) => {
